9f9f1a8e0b2623bc2c665a45587fc1aa1b3c4b35
pikitosdoritos <fastovetsn@gmail.com>
2026-07-06 20:43:56 +0300

    refactor output fields for better accessibility and styling

--- web/script.js
+++ web/script.js
@@ -3,5 +3,11 @@ window.onchange = handleFieldSelection
 function handleFieldSelection(e) {
     if (!e.target.matches("select")) return
     
+    const select = e.target
+    const selected = Array.from(select.selectedOptions).map(o => o.value)
+    const form = document.getElementById(select.name)
     
+    for (const div of form.children) {
+        div.hidden = !selected.includes(div.dataset.name)        
+    }
 }
