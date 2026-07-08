65871ccc0cf12e4928313cb60aae4e15df1a30bf
pikitosdoritos <fastovetsn@gmail.com>
2026-07-08 20:06:47 +0300

    fix button form reference in index.html and improve script.js structure

--- web/script.js
+++ web/script.js
@@ -1,13 +1,48 @@
 window.onchange = handleFieldSelection
 
+const form = document.getElementById('inputs')
+const calcMap = {
+    monthly: [
+        ['monthly'],
+        ['daily', 'dpm'],
+        ['hourly', 'dpm', 'hpd'],
+    ],
+    daily: [
+        ['daily'],
+        ['monthly', 'dpm'],
+        ['hourly', 'hpd'],
+    ],
+    hourly: [
+        ['hourly'],
+        ['daily', "hpd"],
+        ['monthly', 'dpm', 'hpd'],
+    ],
+    dpm: [
+        ['dpm'],
+        ['monthly', 'daily'],
+        ['monthly', 'hourly', 'hpd'],
+    ],
+    hpd: [
+        ['hpd'],
+        ['daily', 'hourly'],
+        ['mothly', 'dpm', 'hourly'],
+    ],
+}
+
+form.onsubmit = handleSubmit
+
 function handleFieldSelection(e) {
-    if (!e.target.matches("select")) return
-    
+    if (!e.target.matches('select')) return
+
     const select = e.target
     const selected = Array.from(select.selectedOptions).map(o => o.value)
     const form = document.getElementById(select.name)
-    
+
     for (const div of form.children) {
-        div.hidden = !selected.includes(div.dataset.name)        
+        div.hidden = !selected.includes(div.dataset.name)
     }
+}
+
+function handleSubmit() {
+
 }
